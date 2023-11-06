4848a25f62f6fab55bf91c9901742536a6e7fec6
Tamanna <tamannakhanum@hotmail.co.uk>
2023-11-06 11:44:30 +0000

    frogger

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,11 @@
 // grid
 // cells
 
+const grid = document.querySelector('.grid')
+const startButton = document.getElementById('startButton')
+
+
+
 // ! VARIABLES
 
 // set game score (if different levels added)
@@ -10,12 +15,62 @@
 // set starting position of frog as const
 // set initial position of each obstacle
 
+const gameContainer = document.createElement('div')
+gameContainer.classList.add('game')
+document.body.appendChild(gameContainer)
+
+const game = document.querySelector('.game')
+
+const baby = document.createElement('div')
+baby.classList.add('baby')
+game.appendChild(baby)
+
+const car1 = document.createElement('div')
+car1.classList.add('car1')
+game.appendChild(car1)
+
+const car2 = document.createElement('div')
+car2.classList.add('car2')
+game.appendChild(car2);
+
+const van = document.createElement('div')
+van.classList.add('van')
+game.appendChild(van)
+
+const cyclist = document.createElement('div')
+cyclist.classList.add('cyclist')
+game.appendChild(cyclist)
+
+let babyCell = 45
+let babyLives = 5
+
+const car1Positions = [28, 29, 31, 32]
+const car2Positions = [15, 17, 19]
+const vanPositions = [7, 9, 10, 12, 13]
+const cyclistPositions = [35, 38, 41]
+
 // ! GRID
 
 // create grid
 // 7*7 grid- set width
 // frog and obstacles on grid
 
+const width = 7
+const cellCount = width * width 
+
+function createGrid() {
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement('div')
+    cell.innerText = i
+    cell.id = i
+    cell.style.width = `${100 / width}%`
+    cell.style.height = `${100 / width}%`
+    grid.append(cell)
+  
+  }
+}
+
+createGrid()
 // ! EXECUTIONS
 
 // * main game loop (setInterval) when lives are greater than 0- use keypress event using user input
@@ -48,4 +103,4 @@
 // * Features
   // music, sound effects, transitions.
 
-  
+  createGrid()
